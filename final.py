0bca82bb115125fb95ab18293016a52f74d80639
Mohit Singh Sinsniwal <mohit.sinsniwal@gmail.com>
2023-04-23 03:12:29 +0530

    Added Doc String and Comments

--- final.py
+++ final.py
@@ -5,30 +5,57 @@ from tools import send_email
 import discord
 from discord.ext import commands
 import re
-import math
-import requests
-import json
 
 config = configparser.ConfigParser()
 config.read('../config.ini')
+
+# Fetch the token from the config file
 TOKEN = config['BOT']['token']
-OPENCAGE = config['BOT']['opencage']
 
 
 class Verification(ui.Modal, title='Verfication Link' ):
+    """
+    A UI modal that prompts the user to enter their IITMadras Roll number and
+    sends a verification email containing a unique verification link to the
+    user's email address.
+
+    Methods:
+        on_submit: Called when the user submits the modal. If the user's Roll
+            number is valid, assigns the appropriate roles to the user and sends
+            a verification email containing a unique verification link to the
+            user's email address.
+        on_timeout: Called when the user fails to submit the modal within the
+            specified time limit.
+        on_error: Called if there is an error while processing the user's
+            submission.
+
+    """
     roll = ui.TextInput(label='Enter Roll Number')
 
     async def on_submit(self, interaction: discord.Interaction):
+        """
+        Called when the user submits the verification modal.
+
+        If the user's Roll number is valid, assigns the appropriate roles to the
+        user and sends a verification email containing a unique verification link
+        to the user's email address.
+        """
+        # Retrieve the Fernet Key from the config file and create a Fernet cipher
         Key = config['Key']['fernet'].encode()
         cipher = Fernet(Key)
+
+        # Retrieve the user's Roll number from the text input field and the user's ID from the interaction object
         userRoll = self.roll.value
         userID = str(interaction.user.id)
+
+        # Combine the user's Roll number and ID and encrypt it using the Fernet cipher
         data = userRoll+'|'+userID
         data = data.encode()
         enc = cipher.encrypt(data)
 
+        # Check if the Roll number is valid
         if re.fullmatch('[0-9][0-9][a-z]*[0-9]*', userRoll) and len(userRoll) in [10, 11]:
-
+            # Assign the appropriate roles to the user based on their number of tries.
             dotOne = discord.utils.get(
                 interaction.guild.roles, id=1078208692853420073)
             if dotOne in interaction.user.roles:
@@ -47,22 +74,39 @@ class Verification(ui.Modal, title='Verfication Link' ):
                     await interaction.user.add_roles(dotTwo)
             else:
                 await interaction.user.add_roles(dotOne)
+
+            # Send a verification email containing a unique verification link to the user's email address
             send_email(interaction.user.name, userRoll, enc)
+
+            # Send a message to the user indicating that a verification link has been sent to their email address
             await interaction.response.send_message(f"Please check your email inbox for a link that has been sent to your email address, {userRoll}@ds.study.iitm.ac.in.", ephemeral=True)
         else:
+            # Send a message to the user indicating that their Roll number is invalid
             await interaction.response.send_message(f"For the system to process your request, we require you to enter your official IITMadras Roll number.", ephemeral=True)
 
     async def on_timeout(self, interaction: discord.Interaction) -> None:
+        """
+        Called when the user fails to submit the modal within the specified time limit.
+        """
         await interaction.response.send_message("We apologize, but your session has expired. Please try again and ensure that you enter your email within 5 minutes.", ephemeral=True)
         return
 
     async def on_error(self, interaction: discord.Interaction, error: Exception, ) -> None:
+        """
+        Called if there is an error while processing the user's submission.
+        """
         await interaction.response.send_message("We apologize for the inconvenience. Please contact a moderator and inform them about the error you encountered so that we can fix it.", ephemeral=True)
         return
 
 
 class Menu(discord.ui.View):
+    """
+    A Discord UI view that displays a menu for EMAIL VERIFICATION.
+    """
     def __init__(self) -> None:
+        """
+        Initializes the menu view and adds a 'Verify' button to the view.
+        """
         super().__init__()
         self.add_item(discord.ui.Button(
             label="Verify", custom_id='verify_email', style=discord.ButtonStyle.blurple))
@@ -79,19 +123,31 @@ async def on_ready():
 
 @bot.event
 async def on_message(message: discord.Message):
-    if message.author.id == 730762300503490650:
+    """
+    An event function that triggers whenever a new message is sent in the server. It updates user roles based on the message
+    contents and also sends a message when a specific user sends a message.
+    """
+    # Admin can create a new verification modal by sending a message with the prefix ^create
+    if message.author.id == 730762300503490650: # ID of Admin
         if message.content[0:7] == '^create':
             await message.channel.send("Join our exclusive community and gain access to private channels and premium content by verifying your email address. Click the button below to complete the process and unlock all the benefits of being a part of our server.", view=Menu())
-    elif message.author.id == 1078142811725123594:
-        # read it
+    
+
+
+    # When somes verfies there email address by clicking the verification link, the email
+    # verification script sends a message to the server in the format: user_id|roll|old_user
+    elif message.author.id == 1078142811725123594: # ID of ServerBot
         data = message.content
         await message.delete()
-        # delete message
-        # update roles
+
+        # Extract the user's ID, Roll number and old username from the message
         user_id, roll, old_user = data.split("|")
         user_id = int(user_id)
-        guild = bot.get_guild(762774569827565569)
+
+        guild = bot.get_guild(762774569827565569) # ID of the server
         user = guild.get_member(user_id)
+        
+        # Remove all the roles from the user, except the @everyone role
         for role in user.roles[1:]:
             await user.remove_roles(role)
         if roll[2] == 'f':
@@ -106,6 +162,8 @@ async def on_message(message: discord.Message):
             Science = 924703232817770497
             role = discord.utils.get(guild.roles, id=Science)
             await user.add_roles(role)  # Diploma Science
+
+        # If other users using the same email address are present in the server, remove their roles
         if old_user != 'None':
             if old_user != str(user_id):
                 old_user = int(old_user)
@@ -120,12 +178,17 @@ async def on_message(message: discord.Message):
 
 @bot.event
 async def on_interaction(interaction: discord.Interaction):
+    """
+    Handles user interactions with the bot, specifically those related to verifying email addresses
+    """
     if interaction.type == discord.InteractionType.component:
         if interaction.data["custom_id"] == "verify_email":
+            # Get the required roles for verification and spam prevention
             Qualifier = discord.utils.get(
                 interaction.guild.roles, id=780935056540827729)
             spam = discord.utils.get(
                 interaction.guild.roles, id=1078208518793994240)
+            # Check if user has already been verified and is not marked as a spammer
             if Qualifier in interaction.user.roles:
                 if spam not in interaction.user.roles:
                     await interaction.response.send_modal(Verification())
@@ -137,6 +200,12 @@ async def on_interaction(interaction: discord.Interaction):
 
 @bot.event
 async def on_raw_reaction_add(payload):
+    """
+    React to a message with a specific emoji to assign or remove roles from a member.
+    If the emoji is 🇩, remove all subject roles from the member and add the roles "Foundational Alumni" and "Diploma".
+    If the emoji is 🥋, remove the role "Diploma" from the member and add the role "Foundational" if the member doesn't have it already.
+    If the emoji is 🇧, remove all subject roles, "Foundational", and "Diploma" from the member and add the roles "Foundational Alumni", "Diploma Alumni", and "BSc".
+    """
     if payload.guild_id is None:
         return  # Reaction is on a private message
 
@@ -208,6 +277,11 @@ async def on_raw_reaction_add(payload):
 
 @bot.event
 async def on_raw_reaction_remove(payload):
+    """
+    If the emoji is 🇩, remove all subject roles from the member and add the roles "Foundational Alumni" and "Diploma".
+    If the emoji is 🥋, remove the role "Diploma" from the member and add the role "Foundational" if the member doesn't have it already.
+    If the emoji is 🇧, remove all subject roles, "Foundational", and "Diploma" from the member and add the roles "Foundational Alumni", "Diploma Alumni", and "BSc".
+    """
     if payload.guild_id is None:
         return
         
--- tools.py
+++ tools.py
@@ -4,7 +4,7 @@ import configparser
 from itertools import cycle
 from discord.ext import tasks
 
-
+# Attempt to read the configuration file and retrieve the SendinBlue API key.
 try:
     config = configparser.ConfigParser()
     config.read('../config.ini')
@@ -13,92 +13,33 @@ except:
     config = configparser.ConfigParser()
     config.read('config.ini')
 SIB_API_KEY=config['send_in_blue']['sib_api_key']
-REST_API_HOSTNAME=config['ibm_cloud_db2']['rest_api_hostname']
-DEPLOYMENT_ID=config['ibm_cloud_db2']['deployment_id']
+
+# Attempt to read the configuration file and retrieve the Discord bot token.
 TOKEN=config['BOT']['token']
+
+# Set the activity status for the Discord bot.
 activity= cycle([discord.Activity(type=discord.ActivityType.watching,name='Donnie Darko'),discord.Activity(type=discord.ActivityType.listening,name='Traffic')])
 
 
 def send_email(name:str,roll_no:str,link:bytes): 
+    """
+    This function sends an email to the user with the verification link.
+    :param name: The name of the user.
+    :param roll_no: The roll number of the user.
+    :param link: The verification link.
+    :return: None
+    """
+    # Set up the configuration for the SendinBlue API.
     configuration = sib_api_v3_sdk.Configuration()
     configuration.api_key['api-key'] = SIB_API_KEY
     api_instance = sib_api_v3_sdk.TransactionalEmailsApi(sib_api_v3_sdk.ApiClient(configuration))
+    
+	# Set up the email content.
     senderSmtp = sib_api_v3_sdk.SendSmtpEmailSender(name="IITM Discord Server",email=config['send_in_blue']['sender_email'])
     link=link.decode()
-    htmlcontent="""<!DOCTYPE html>
-<html>
-<head>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Discord Verification</title>
-	<!-- Bootstrap CSS -->
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
-	<style type="text/css">
-		body {
-			font-family: 'Arial', sans-serif;
-			font-size: 14px;
-			line-height: 1.6;
-			margin: 0;
-			padding: 0;
-		}
-		.container {
-			max-width: 600px;
-			margin: 0 auto;
-			padding: 20px;
-			background-color: rgba(255, 255, 255, 0.9);
-			border-radius: 5px;
-			box-shadow: 0px 0px 10px #d9d9d9;
-		}
-		h1 {
-			font-size: 36px;
-			font-weight: bold;
-			margin-bottom: 30px;
-			text-align: center;
-			color: #424242;
-			text-shadow: 2px 2px 3px rgba(0,0,0,0.3);
-		}
-		p {
-			margin-bottom: 20px;
-			text-align: justify;
-			color: #424242;
-			text-shadow: 1px 1px 2px rgba(0,0,0,0.2);
-		}
-		a {
-			color: #ffffff;
-			text-decoration: none;
-		}
-		.button {
-			display: inline-block;
-			background-color: #7289DA;
-			border-radius: 4px;
-			color: #ffffff;
-			font-size: 18px;
-			font-weight: bold;
-			margin-top: 20px;
-			padding: 10px 20px;
-			text-align: center;
-			text-decoration: none;
-			transition: background-color 0.3s ease;
-		}
-		.button:hover {
-			background-color: #677bc4;
-		}
-	</style>
-</head>
-<body>"""+f"""
-	<div class="container">
-		<h1>Access Private Channels - Verify Your Account on IITM Discord</h1>
-		<p>Hello {name},</p>
-		<p>Thank you for joining the IITM Discord Server, the ultimate destination for students to connect, collaborate, and exchange ideas with like-minded individuals.</p>
-		<p>To gain access to our private channels and unlock the full range of resources available on our server, please click the button below to verify your account:</p>
-		<p>If you did not request this verification, you can safely ignore this email. Someone else might have typed your email address by mistake.</p>
-		<a href="https://sinsniwal.me/discord-iitm/verify/{link}" class="button">Verify my account</a>
-		<p>Thank you for your participation in our community.</p>
-		<p>Thanks!<br>Server Bot Developers</p>
-	</div>
-</body>
-</html>
-"""
+    with open('email_template.txt','r') as f:
+        htmlcontent=f.read()
+    htmlcontent=htmlcontent.replace('{name}',name)
     subject="Verify Your IITM Discord Account Now"
     sendTo = sib_api_v3_sdk.SendSmtpEmailTo(email=roll_no+'@ds.study.iitm.ac.in',name=name)
     arrTo = [sendTo] #Adding `to` in a list
@@ -107,4 +48,6 @@ def send_email(name:str,roll_no:str,link:bytes):
         to=arrTo,
         html_content=htmlcontent,
         subject=subject) # SendSmtpEmail | Values to send a transactional email
+    
+    # Send the email using the SendinBlue API.
     api_response = api_instance.send_transac_email(send_smtp_email)
