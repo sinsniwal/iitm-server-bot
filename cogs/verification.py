8b9bec8444e48883afecda3eb43dcb7e996cbed9
siddharth <siddharthn02@gmail.com>
2023-05-16 01:33:42 +0530

    verification.py & helpers

---
+++ cogs/verification.py
@@ -0,0 +1,86 @@
+import config
+import discord
+from discord.ext import commands
+
+from utils.helper import admin_only, verification_embed_dm
+
+
+class Menu(discord.ui.View):
+    """
+    A Discord UI view that displays a menu for EMAIL VERIFICATION.
+    """
+    def __init__(self) -> None:
+        """
+        Initializes the menu view and adds a 'Verify' button to the view.
+        """
+        super().__init__()
+        self.add_item(discord.ui.Button(
+            label="Verify", custom_id='verify_email', style=discord.ButtonStyle.blurple))
+
+
+class Verification(commands.Cog):
+
+    def __init__(self,bot) -> None:
+        self.bot = bot
+
+    @commands.command()
+    @admin_only()
+    async def create(self,ctx):
+        await ctx.channel.send("Join our exclusive community and gain access to private channels and premium content by verifying your email address. Click the button below to complete the process and unlock all the benefits of being a part of our server.", view=Menu())
+
+    @commands.command()
+    @admin_only()
+    async def send(self, ctx):
+        embed=verification_embed_dm()
+        await ctx.author.send(embed=embed)
+
+
+    @commands.Cog.listener()
+    async def on_message(self, message):
+        if message.channel.id != config.AUTOMATE_CHANNEL:
+            return
+        if message.author.id == self.bot.user.id: # ID of ServerBot
+            data = message.content
+            await message.delete()
+
+            # Extract the user's ID, Roll number and old username from the message
+            user_id, roll, old_user = data.split("|")
+            user_id = int(user_id)
+
+            guild = self.bot.get_guild(762774569827565569) # ID of the server
+            user = guild.get_member(user_id)
+            
+            # Remove all the roles from the user, except the @everyone role
+            for role in user.roles[1:]:
+                await user.remove_roles(role)
+            if roll[2] == 'f':
+                Foundational = 780875583214321684
+                role = discord.utils.get(guild.roles, id=Foundational)
+                await user.add_roles(role)  # Foundational
+            elif roll[3] == 'p':
+                Programming = 924703833693749359
+                role = discord.utils.get(guild.roles, id=Programming)
+                await user.add_roles(role)  # Diploma Programming
+            elif roll[3] == 's':
+                Science = 924703232817770497
+                role = discord.utils.get(guild.roles, id=Science)
+                await user.add_roles(role)  # Diploma Science
+
+            # If other users using the same email address are present in the server, remove their roles
+            if old_user != 'None':
+                if old_user != str(user_id):
+                    old_user = int(old_user)
+                    Qualifier = 780935056540827729
+                    Qualifier = discord.utils.get(guild.roles, id=Qualifier)
+                    mem = guild.get_member(old_user)
+                    if mem:
+                        for role in mem.roles[1:]:
+                            await mem.remove_roles(role)
+                        await mem.add_roles(Qualifier)  # Qualifier
+
+            # Send DM to the user
+            embed = verification_embed_dm()
+            await user.send(embed=embed)
+
+async def setup(bot):
+    await bot.add_cog(Verification(bot))
---
+++ utils/helper.py
@@ -0,0 +1,83 @@
+import os
+import config
+
+from datetime import datetime
+from itertools import cycle
+
+import sib_api_v3_sdk
+
+import discord
+from discord.ext import commands
+
+
+# Attempt to read the configuration file and retrieve the SendinBlue API key.
+# try:
+#     config = configparser.ConfigParser()
+#     config.read('../config.ini')
+#     SIB_API_KEY=config['send_in_blue']['sib_api_key']
+# except:
+#     config = configparser.ConfigParser()
+#     config.read('config.ini')
+
+SIB_API_KEY=os.environ.get("SIB_API_KEY")
+
+# Attempt to read the configuration file and retrieve the Discord bot token.
+FERNETKEY = os.environ.get("FERNET")
+
+# Set the activity status for the Discord bot.
+activity= cycle([discord.Activity(type=discord.ActivityType.watching,name='Donnie Darko'),discord.Activity(type=discord.ActivityType.listening,name='Traffic')])
+
+def admin_only():
+    async def pred(ctx: commands.Context):
+        if ctx.author.id != config.ADMIN_ID:
+            raise commands.CheckFailure()
+        return True
+    return commands.check(pred)
+
+
+def verification_embed_dm():
+    embed = discord.Embed(title="Verification Complete - You're In!",
+                          url="https://discord.com/servers/iitm-bs-students-762774569827565569",
+                          description="Optimize Your College Server Experience: Get Your Roles and Access Private Channels Tailored to Your Interests! Update Your Level and Get Your Club Roles and Subject Roles in Corresponding Categories. Gain Access to Private Channels and Engage with Like-minded Peers\n\n> Message from ServerBot\n```\nIf you're familiar with discord.py, you can get the server developer role. Check out my GitHub repo link and send a pull request to enhance my functionality.\n```[Github Repo](https://bit.ly/IITMbot)",
+                          colour=0x00b0f4,
+                          timestamp=datetime.now())
+    embed.set_author(name="IITM BS Students",
+                     url="https://discord.com/servers/iitm-bs-students-762774569827565569",
+                     icon_url="https://cdn.discordapp.com/icons/762774569827565569/a_c1c0b26032fa931e5530abd0fbf0b14f.gif?size=128")
+
+    embed.set_image(
+        url="https://i.ibb.co/mFP3KtR/833070-pxplcgyzbd-1490711385.jpg")
+    embed.set_footer(text="Have fun ✌️")
+    return embed
+
+def send_email(name:str,roll_no:str,link:bytes): 
+    """
+    This function sends an email to the user with the verification link.
+    :param name: The name of the user.
+    :param roll_no: The roll number of the user.
+    :param link: The verification link.
+    :return: None
+    """
+    # Set up the configuration for the SendinBlue API.
+    configuration = sib_api_v3_sdk.Configuration()
+    configuration.api_key['api-key'] = SIB_API_KEY
+    api_instance = sib_api_v3_sdk.TransactionalEmailsApi(sib_api_v3_sdk.ApiClient(configuration))
+    
+	# Set up the email content.
+    senderSmtp = sib_api_v3_sdk.SendSmtpEmailSender(name="IITM Discord Server",email=os.environ.get("SIB_SENDER_EMAIL"))
+    link=link.decode()
+    print(link)
+    with open('utils/email_template.txt','r') as f:
+        htmlcontent=f.read()
+    htmlcontent=htmlcontent.replace('{name}',name).replace('{link}',link)
+    subject="Verify Your IITM Discord Account Now"
+    sendTo = sib_api_v3_sdk.SendSmtpEmailTo(email=roll_no+'@ds.study.iitm.ac.in',name=name)
+    arrTo = [sendTo] #Adding `to` in a list
+    send_smtp_email = sib_api_v3_sdk.SendSmtpEmail(
+        sender=senderSmtp,
+        to=arrTo,
+        html_content=htmlcontent,
+        subject=subject) # SendSmtpEmail | Values to send a transactional email
+    
+    # Send the email using the SendinBlue API.
+    api_response = api_instance.send_transac_email(send_smtp_email)
