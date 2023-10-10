26dac95b772931047bb4057d603bb9fa02ac9070
Mohit Singh Sinsniwal <mohit.sinsniwal@gmail.com>
2023-10-10 14:42:24 +0530

    Including ES and oauth

--- cogs/interaction.py
+++ cogs/interaction.py
@@ -2,16 +2,13 @@ from __future__ import annotations
 
 import logging
 import os
-import re
 from typing import TYPE_CHECKING
 
 import discord
 from cryptography.fernet import Fernet
-from discord import ui
 from discord.ext import commands
-
+from datetime import datetime
 import config
-from utils.helper import send_email
 
 
 if TYPE_CHECKING:
@@ -19,117 +16,6 @@ if TYPE_CHECKING:
 
 
 logger = logging.getLogger("Email Verification Modal")
-
-
-class Verification(ui.Modal, title="Verfication Link"):
-    """
-    A UI modal that prompts the user to enter their IITMadras Roll number and
-    sends a verification email containing a unique verification link to the
-    user's email address.
-
-    Methods:
-        on_submit: Called when the user submits the modal. If the user's Roll
-            number is valid, assigns the appropriate roles to the user and sends
-            a verification email containing a unique verification link to the
-            user's email address.
-        on_timeout: Called when the user fails to submit the modal within the
-            specified time limit.
-        on_error: Called if there is an error while processing the user's
-            submission.
-
-    """
-
-    roll = ui.TextInput(label="Enter Roll Number")
-
-    async def on_submit(self, interaction: discord.Interaction):
-        """
-        Called when the user submits the verification modal.
-
-        If the user's Roll number is valid, assigns the appropriate roles to the
-        user and sends a verification email containing a unique verification link
-        to the user's email address.
-        """
-        assert interaction.guild
-        assert isinstance(interaction.user, discord.Member)
-        logger.info("on_submit")
-        # Retrieve the Fernet Key from the config file and create a Fernet cipher
-        cipher = Fernet(os.environ["FERNET"])
-
-        logger.info(cipher)
-
-        # Retrieve the user's Roll number from the text input field and the user's ID from the interaction object
-        user_roll = self.roll.value.strip()
-        user_id = str(interaction.user.id)
-
-        # Combine the user's Roll number and ID and encrypt it using the Fernet cipher
-        data = user_roll + "|" + user_id
-        data = data.encode()
-        enc = cipher.encrypt(data)
-        # Check if the Roll number is valid
-        # ignore the case of the Roll number and check if it matches the regex pattern using re.LOWERCASE
-        if re.fullmatch("[0-9][0-9][a-z]*[0-9]*", user_roll, re.IGNORECASE) and len(user_roll) in [10, 11]:
-            # Assign the appropriate roles to the user based on their number of tries.
-            dot_one = discord.utils.get(interaction.guild.roles, id=config.DOT_ONE_ROLE)
-            if not dot_one:
-                return
-            if dot_one in interaction.user.roles:
-                dot_two = discord.utils.get(interaction.guild.roles, id=config.DOT_TWO_ROLE)
-                if not dot_two:
-                    return
-                if dot_two in interaction.user.roles:
-                    dot_three = discord.utils.get(interaction.guild.roles, id=config.DOT_THREE_ROLE)
-                    if not dot_three:
-                        return
-                    if dot_three in interaction.user.roles:
-                        spam = discord.utils.get(interaction.guild.roles, id=config.SPAM_ROLE)
-                        if not spam:
-                            return
-                        await interaction.user.add_roles(spam)
-                    else:
-                        await interaction.user.add_roles(dot_three)
-                else:
-                    await interaction.user.add_roles(dot_two)
-            else:
-                await interaction.user.add_roles(dot_one)
-
-            # Send a verification email containing a unique verification link to the user's email address
-            send_email(interaction.user.name, user_roll, enc)
-
-            # Send a message to the user indicating that a verification link has been sent to their email address
-            await interaction.response.send_message(
-                f"Please check your email inbox for a link that has been sent to your email address, {user_roll}@ds.study.iitm.ac.in.",
-                ephemeral=True,
-            )
-        else:
-            # Send a message to the user indicating that their Roll number is invalid
-            await interaction.response.send_message(
-                f"For the system to process your request, we require you to enter your official IITMadras Roll number.",
-                ephemeral=True,
-            )
-
-    async def on_timeout(self, interaction: discord.Interaction) -> None:
-        """
-        Called when the user fails to submit the modal within the specified time limit.
-        """
-        await interaction.response.send_message(
-            "We apologize, but your session has expired. Please try again and ensure that you enter your email within 5 minutes.",
-            ephemeral=True,
-        )
-        return
-
-    async def on_error(
-        self,
-        interaction: discord.Interaction,
-        error: Exception,
-    ) -> None:
-        """
-        Called if there is an error while processing the user's submission.
-        """
-        await interaction.response.send_message(
-            f"We apologize for the inconvenience. Please contact a moderator and inform them about the error you encountered so that we can fix it: {error}",
-            ephemeral=True,
-        )
-        return
 
 
 class Interaction(commands.Cog):
@@ -153,10 +39,55 @@ class Interaction(commands.Cog):
                 # Check if user has already been verified and is not marked as a spammer
                 if qualifier in interaction.user.roles:
                     if spam not in interaction.user.roles:
-                        await interaction.response.send_modal(Verification())
+                        cipher = Fernet(os.environ["FERNET"])
+                        logger.info(cipher)
+                        # Retrieve the user's ID from the interaction object
+                        user_id = str(interaction.user.id)
+                        timestamp = int(datetime.now().timestamp())
+                        # Combine the user's ID and timestamp and encrypt it using the Fernet cipher
+                        data = user_id + "|" + str(timestamp)
+                        data = data.encode()
+                        enc = cipher.encrypt(data)
+                        # Check if the Roll number is valid
+                        # ignore the case of the Roll number and check if it matches the regex pattern using re.LOWERCASE
+                        # Assign the appropriate roles to the user based on their number of tries.
+                        baseURL = f"https://sinsniwal.me/discord-iitm/g-oauth-login/{enc.decode()}"
+                        dot_one = discord.utils.get(interaction.guild.roles, id=config.DOT_ONE_ROLE)
+                        if not dot_one:
+                            return
+                        if dot_one in interaction.user.roles:
+                            dot_two = discord.utils.get(interaction.guild.roles, id=config.DOT_TWO_ROLE)
+                            if not dot_two:
+                                return
+                            if dot_two in interaction.user.roles:
+                                dot_three = discord.utils.get(interaction.guild.roles, id=config.DOT_THREE_ROLE)
+                                if not dot_three:
+                                    return
+                                if dot_three in interaction.user.roles:
+                                    spam = discord.utils.get(interaction.guild.roles, id=config.SPAM_ROLE)
+                                    if not spam:
+                                        return
+                                    await interaction.user.add_roles(spam)
+                                else:
+                                    await interaction.user.add_roles(dot_three)
+                            else:
+                                await interaction.user.add_roles(dot_two)
+                        else:
+                            await interaction.user.add_roles(dot_one)
+                        embed = discord.Embed()
+                        embed.description = (
+                            f"Do Not Share This Link With Anyone.Click [here]({baseURL}) to verify using google oauth."
+                        )
+                        await interaction.response.send_message(
+                            embed=embed,
+                            ephemeral=True,
+                        )
+
+                        # f"Do Not Share This Link With Anyone. Click on the link below to complete the verification process for your account. {baseURL}",
+
                     else:
                         await interaction.response.send_message(
-                            "Please check your email for a verification link that was previously sent to you. Click on this link to complete the verification process for your account.",
+                            "Limit Reached. Please contact the server staff to increase your limit.",
                             ephemeral=True,
                         )
                 else:
--- cogs/verification.py
+++ cogs/verification.py
@@ -30,11 +30,17 @@ class Menu(discord.ui.View):
 class Verification(commands.Cog):
     def __init__(self, bot: IITMBot) -> None:
         self.bot = bot
-        self._roles = {
+        self.entry_roles = {
             "f": 780875583214321684,  # Foundational
             "p": 924703833693749359,  # Diploma Programming
             "s": 924703232817770497,  # Diploma Science
             "_": 780935056540827729,  # Qualifier
+        }
+        self.branch_roles = {
+            "ef": 1161202924148490290,  # ES Foundational
+            "df": 1161203200041422869,  # DS Foundational
+            "es": 1129961617694674996,  # ES Student
+            "ds": 1129961686917460029,  # DS Student
         }
 
     @commands.command()
@@ -60,8 +66,8 @@ class Verification(commands.Cog):
             data = message.content
             await message.delete()
 
-            # Extract the user's ID, Roll number and old username from the message
-            user_id, roll, old_user = data.split("|")
+            # Extract the user's ID, Email ID and old username from the message
+            user_id, email, old_user = data.split("|")
             user_id = int(user_id)
 
             guild = self.bot.get_guild(762774569827565569)  # ID of the server
@@ -70,26 +76,43 @@ class Verification(commands.Cog):
             if user is None:
                 return
 
+            branch = email.split("@")[1][:2].lower()
+            if branch == "ds":
+                branch_id = self.branch_roles["df"]
+            elif branch == "es":
+                branch_id = self.branch_roles["ef"]
+            else:
+                branch_id = self.entry_roles["_"]
+            await user.add_roles(discord.Object(branch_id))
             # sacrifice a *little* bit of readability in the name of EAFP
             # also, exception handling is free since 3.11
             try:
-                role_id = self._roles[roll[2]]
+                role_id = self.entry_roles[email[2]]
             except KeyError:
                 try:
-                    role_id = self._roles[roll[3]]
+                    role_id = self.entry_roles[email[3]]
                 except KeyError:
                     # shouldn't happen, but default to Qualifier just in case
-                    role_id = self._roles["_"]
-
-            await user.edit(roles=[discord.Object(role_id)])
-
+                    role_id = self.entry_roles["_"]
+            # remove roles like qualifier and interested or ds es qualifiers, if they exist
+            # add the roles to the user
+            await user.add_roles(discord.Object(role_id))
+            await user.remove_roles(discord.Object(780935056540827729))  # remove qualifier
+            await user.remove_roles(discord.Object(764800631386537994))  # remove interested role
+            try:
+                await user.remove_roles(discord.Object(1078208692853420073))
+                await user.remove_roles(discord.Object(1078208892296761404))
+                await user.remove_roles(discord.Object(1078208973326536724))
+                await user.remove_roles(discord.Object(1078208518793994240))
+            except:
+                pass
             # If other users using the same email address are present in the server, remove their roles
             if old_user != "None":
                 if old_user != str(user_id):
                     old_user = int(old_user)
                     mem = guild.get_member(old_user)
                     if mem:
-                        await mem.edit(roles=[discord.Object(self._roles["_"])])  # Qualifier
+                        await mem.edit(roles=[discord.Object(self.entry_roles["_"])])  # Qualifier
 
             # Send DM to the user
             embed = verification_embed_dm()
